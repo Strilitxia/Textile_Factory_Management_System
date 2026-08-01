a104fb3a3253953df21b6e32e51e5ef9b5ac2ebe
dilir-diyanat <dilirdiyanat@gmail.com>
2026-08-01 16:09:17 +0600

    Added Hr Manager Controllers

---
+++ src/main/java/com/example/textile_factory_management_system/dilir/HrManager/Controller/ViewWorkerAttendanceController.java
@@ -0,0 +1,7 @@
+package com.example.textile_factory_management_system.dilir.HrManager.Controller;
+
+public class ViewWorkerAttendanceController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/com/example/textile_factory_management_system/dilir/HrManager/Controller/WorkerEvaluationReportsController.java
@@ -0,0 +1,7 @@
+package com.example.textile_factory_management_system.dilir.HrManager.Controller;
+
+public class WorkerEvaluationReportsController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
