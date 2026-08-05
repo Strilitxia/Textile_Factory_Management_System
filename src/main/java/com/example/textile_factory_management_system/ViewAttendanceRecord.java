1d5759a5705e5b4ec933091e99241a78ad0a060f
amanna-mahi <amannabinteahsanmahi@gmail.com>
2026-08-05 17:42:43 +0600

    added and modified

---
+++ src/main/java/com/example/textile_factory_management_system/ViewAttendanceRecord.java
@@ -0,0 +1,23 @@
+package com.example.textile_factory_management_system;
+
+public class ViewAttendanceRecord
+{
+    @javafx.fxml.FXML
+    private TableColumn datesTC;
+    @javafx.fxml.FXML
+    private TableView tableViewTV;
+    @javafx.fxml.FXML
+    private TableColumn clockOutTC;
+    @javafx.fxml.FXML
+    private Button myAttendanceButtonOA;
+    @javafx.fxml.FXML
+    private TableColumn clockinTC;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void myAttendanceButtonOA(ActionEvent actionEvent) {
+    }
+}
