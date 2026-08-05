52142686423b7f4a4a43a2389706a96537056527
amanna-mahi <amannabinteahsanmahi@gmail.com>
2026-08-05 19:10:08 +0600

    added and modified

---
+++ src/main/java/com/example/textile_factory_management_system/AssignedShiftTimeAttendanceViewController.java
@@ -0,0 +1,7 @@
+package com.example.textile_factory_management_system;
+
+public class AssignedShiftTimeAttendanceViewController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/com/example/textile_factory_management_system/SubmitPersonalLeaveRequestViewController.java
@@ -0,0 +1,24 @@
+package com.example.textile_factory_management_system;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class SubmitPersonalLeaveRequestViewController
+{
+    @javafx.fxml.FXML
+    private Text submitPersonalLeaveRequestLabel;
+    @javafx.fxml.FXML
+    private DatePicker leaveDatesDP;
+    @javafx.fxml.FXML
+    private TextField reasonTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void submitButtonOA(ActionEvent actionEvent) {
+    }
+}
