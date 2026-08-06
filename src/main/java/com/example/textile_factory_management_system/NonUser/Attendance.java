efb73815e07cd24cc0d969d54bc619a119aae50e
amanna-mahi <amannabinteahsanmahi@gmail.com>
2026-08-06 11:28:06 +0600

    added non user class

---
+++ src/main/java/com/example/textile_factory_management_system/NonUser/Attendance.java
@@ -0,0 +1,9 @@
+package com.example.textile_factory_management_system.NonUser;
+
+import java.time.LocalDate;
+
+public class Attendance {
+    private LocalDate timestamp;
+    private int employeeID;
+    private String attendanceType;
+}
