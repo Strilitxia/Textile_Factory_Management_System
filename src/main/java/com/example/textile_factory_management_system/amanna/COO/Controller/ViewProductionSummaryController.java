97a72bfcc58aa4180e832dc2cb9cc26cac0b8e3a
amanna-mahi <amannabinteahsanmahi@gmail.com>
2026-08-07 20:26:07 +0600

    Added generic class

--- src/main/java/com/example/textile_factory_management_system/amanna/COO/Controller/ViewProductionSummaryController.java
+++ src/main/java/com/example/textile_factory_management_system/amanna/COO/Controller/ViewProductionSummaryController.java
@@ -2,21 +2,23 @@ package com.example.textile_factory_management_system.amanna.COO.Controller;
 
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 
 public class ViewProductionSummaryController
 {
     @javafx.fxml.FXML
-    private TableColumn productionOutputIdTc;
+    private TableColumn<ProductionSummary,Integer> productionOutputIdTc;
     @javafx.fxml.FXML
-    private TableColumn batchIdTC;
+    private TableColumn<ProductionSummary,Integer> batchIdTC;
     @javafx.fxml.FXML
     private Text productionSummaryLabel;
     @javafx.fxml.FXML
-    private TableView productionSummaryTV;
+    private TableView<ProductionSummary> productionSummaryTV;
     @javafx.fxml.FXML
-    private TableColumn totalProducedTC;
+    private TableColumn<ProductionSummary,Integer> totalProducedTC;
 
     @javafx.fxml.FXML
     public void initialize() {
+         productionOutputIdTc.setCellValueFactory(new PropertyValueFactory<ProductionSummary,Integer>());
     }}
