2b485fee87ea5af12cc03bd25dae6d847addbf5c
Asif Amin <aminasif313@gmail.com>
2026-08-08 17:46:30 +0600

    completed Buyer G2, G3

--- src/main/java/com/example/textile_factory_management_system/NonUser/Quotation.java
+++ src/main/java/com/example/textile_factory_management_system/NonUser/Quotation.java
@@ -3,8 +3,38 @@ package com.example.textile_factory_management_system.NonUser;
 public class Quotation {
     private int quotationId,rfqId;
     private float proposedUnitPrice;
-    private boolean isRejected;
+    private String productType;
+    private String status;
+
+    public Quotation(int quotationId, int rfqId, float proposedUnitPrice, String productType) {
+        this.quotationId = quotationId;
+        this.rfqId = rfqId;
+        this.proposedUnitPrice = proposedUnitPrice;
+        this.productType = productType;
+        this.status = "Pending";
+    }
+
+    public int getQuotationId() {
+        return quotationId;
+    }
+
+    public int getRfqId() {
+        return rfqId;
+    }
+
+    public float getProposedUnitPrice() {
+        return proposedUnitPrice;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
-
-
-
