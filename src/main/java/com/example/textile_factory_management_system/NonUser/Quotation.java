14ba3e06a9d5dc9980a101b314c4cad497a56319
dilir-diyanat <dilirdiyanat@gmail.com>
2026-08-08 18:25:10 +0600

    updated nonuser quotation and rfq

--- src/main/java/com/example/textile_factory_management_system/NonUser/Quotation.java
+++ src/main/java/com/example/textile_factory_management_system/NonUser/Quotation.java
@@ -4,6 +4,55 @@ public class Quotation {
     private int quotationId,rfqId;
     private float proposedUnitPrice;
     private boolean isRejected;
+
+    public int getQuotationId() {
+        return quotationId;
+    }
+
+    public void setQuotationId(int quotationId) {
+        this.quotationId = quotationId;
+    }
+
+    public int getRfqId() {
+        return rfqId;
+    }
+
+    public void setRfqId(int rfqId) {
+        this.rfqId = rfqId;
+    }
+
+    public float getProposedUnitPrice() {
+        return proposedUnitPrice;
+    }
+
+    public void setProposedUnitPrice(float proposedUnitPrice) {
+        this.proposedUnitPrice = proposedUnitPrice;
+    }
+
+    public boolean isRejected() {
+        return isRejected;
+    }
+
+    public void setRejected(boolean rejected) {
+        isRejected = rejected;
+    }
+
+    public Quotation(int quotationId, int rfqId, float proposedUnitPrice, boolean isRejected) {
+        this.quotationId = quotationId;
+        this.rfqId = rfqId;
+        this.proposedUnitPrice = proposedUnitPrice;
+        this.isRejected = isRejected;
+    }
+
+    @Override
+    public String toString() {
+        return "Quotation{" +
+                "quotationId=" + quotationId +
+                ", rfqId=" + rfqId +
+                ", proposedUnitPrice=" + proposedUnitPrice +
+                ", isRejected=" + isRejected +
+                '}';
+    }
 }
 
 
--- src/main/java/com/example/textile_factory_management_system/NonUser/RFQ.java
+++ src/main/java/com/example/textile_factory_management_system/NonUser/RFQ.java
@@ -3,4 +3,53 @@ package com.example.textile_factory_management_system.NonUser;
 public class RFQ {
     private int rfqId, customerId, productId;
     private float bulkQuantity;
+
+    public int getRfqId() {
+        return rfqId;
+    }
+
+    public void setRfqId(int rfqId) {
+        this.rfqId = rfqId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public float getBulkQuantity() {
+        return bulkQuantity;
+    }
+
+    public void setBulkQuantity(float bulkQuantity) {
+        this.bulkQuantity = bulkQuantity;
+    }
+
+    public RFQ(int rfqId, int customerId, int productId, float bulkQuantity) {
+        this.rfqId = rfqId;
+        this.customerId = customerId;
+        this.productId = productId;
+        this.bulkQuantity = bulkQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return "RFQ{" +
+                "rfqId=" + rfqId +
+                ", customerId=" + customerId +
+                ", productId=" + productId +
+                ", bulkQuantity=" + bulkQuantity +
+                '}';
+    }
 }
